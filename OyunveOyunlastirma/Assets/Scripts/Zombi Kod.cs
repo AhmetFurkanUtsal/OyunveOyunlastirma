2b85a55ba3df21fd102b90b573a61d54b8e4b435
AhmetFurkanUtsal <furkan.utsal@hacettepe.edu.tr>
2024-01-02 20:38:09 +0300

    yabey zeka

--- OyunveOyunlastirma/Assets/Scripts/Zombi Kod.cs
+++ OyunveOyunlastirma/Assets/Scripts/Zombi Kod.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombiKod : MonoBehaviour
 {
@@ -9,14 +10,25 @@ public class ZombiKod : MonoBehaviour
     public float zombiHp = 100;
     Animator zombiAnim;
     bool zombiOlu;
+    public float KocvalamaMesafesi;
+    public float saldirmaMesafesi;
+    float mesafe;
+    NavMeshAgent zombiNavMesh;
+
+
+    GameObject hedefOyuncu;
     void Start()
     {
         zombiAnim = this.GetComponent<Animator>();
+        hedefOyuncu = GameObject.Find("Player");
+        zombiNavMesh = this.GetComponent<NavMeshAgent>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (zombiHp <= 0)
         
         {
@@ -31,9 +43,41 @@ public class ZombiKod : MonoBehaviour
         }
         else
         {
+             mesafe = Vector3.Distance(this.transform.position, hedefOyuncu.transform.position);
+            if(mesafe < KocvalamaMesafesi)
+            {
+
+                zombiNavMesh.isStopped = false;
+                zombiNavMesh.SetDestination(hedefOyuncu.transform.position);
+                // yürüme animasyonu
+                zombiAnim.SetBool("yuruyor", true);
+                zombiAnim.SetBool("saldiriyor", false);
+                this.transform.LookAt(hedefOyuncu.transform.position);
+            }
+            else
+            {
+                zombiNavMesh.isStopped = true;
+                //durma animasyonu
+                zombiAnim.SetBool("yuruyor", false);
+                zombiAnim.SetBool("saldiriyor", false);
+            }
+            if(mesafe < saldirmaMesafesi)
+            {
+                this.transform.LookAt(hedefOyuncu.transform.position);
+                zombiNavMesh.isStopped = true;
+                // vurma animasyonu
+                zombiAnim.SetBool("yuruyor", false);
+                zombiAnim.SetBool("saldiriyor", true);
+            }
+          
 
         }
 
+    }
+
+    public void HasarVer()
+    {
+       // hedefOyuncu.GetComponent<PlayerMovement>().HasarAl();
     }
     IEnumerator YokOl()
     {
